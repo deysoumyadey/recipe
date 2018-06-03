b6f38a2ae2065216b8928a114ad3778f21be5cc4
deysoumyadey <dey.soumyadey@gmail.com>
2018-06-02 20:53:58 -0400

    Entity: UnitOfMeasure added

--- src/main/java/com/som/spring/recipe/domain/Ingredient.java
+++ src/main/java/com/som/spring/recipe/domain/Ingredient.java
@@ -2,7 +2,6 @@ package com.som.spring.recipe.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Set;
 
 @Entity
 public class Ingredient {
@@ -12,7 +11,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -47,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
